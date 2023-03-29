ae3ed7fc8aafaa0fd88df6eb3389b9ae2043a192
Oscar Stenqvist <oscar@Oscars-MacBook-Pro-2.local>
2023-03-29 23:03:52 +0200

    Added more colors

--- main.c
+++ main.c
@@ -2,22 +2,58 @@
 #include <math.h>
 #include <stdio.h>
 #define PI 3.1415926536
-#define WIDTH 1600
-#define HEIGHT 800
-#define MAPX 8
-#define MAPY 8
+#define MAPX 10
+#define MAPY 10
+#define MAPLINE 100
+#define WIDTH 2 * MAPX *MAPLINE
+#define HEIGHT MAPY *MAPLINE
 #define DELAY 6
-#define RAYS 800
+#define RAYS 100
 #define FOV PI / 2
+#define DOF MAPX *MAPY
 struct Mvsqr {
 	float x, y, deltaX, deltaY, angle, width;
 } typedef mvsqr;
+void checkAngleLimits(float *angle) {
+	if (*angle < 0)
+		*angle += 2 * PI;
+	if (*angle > 2 * PI)
+		*angle -= 2 * PI;
+}
+void setColor(SDL_Renderer *renderer, int tileColor, float shade) {
+	switch (tileColor) {
+	case 0:
+		SDL_SetRenderDrawColor(renderer, 0 * shade, 0 * shade, 0 * shade, SDL_ALPHA_OPAQUE);
+		break;
+	case 1:
+		SDL_SetRenderDrawColor(renderer, 255 * shade, 255 * shade, 255 * shade, SDL_ALPHA_OPAQUE);
+		break;
+	case 2:
+		SDL_SetRenderDrawColor(renderer, 0 * shade, 0 * shade, 255 * shade, SDL_ALPHA_OPAQUE);
+		break;
+	case 3:
+		SDL_SetRenderDrawColor(renderer, 0 * shade, 255 * shade, 0 * shade, SDL_ALPHA_OPAQUE);
+		break;
+	case 4:
+		SDL_SetRenderDrawColor(renderer, 255 * shade, 0 * shade, 255 * shade, SDL_ALPHA_OPAQUE);
+		break;
+	case 5:
+		SDL_SetRenderDrawColor(renderer, 255 * shade, 127 * shade, 0 * shade, SDL_ALPHA_OPAQUE);
+		break;
+	case 6:
+		SDL_SetRenderDrawColor(renderer, 255 * shade, 0 * shade, 0 * shade, SDL_ALPHA_OPAQUE);
+		break;
+	case 7:
+		SDL_SetRenderDrawColor(renderer, 255 * shade, 255 * shade, 0 * shade, SDL_ALPHA_OPAQUE);
+		break;
+	}
+}
 void drawRay3D(SDL_Renderer *renderer, int i, int len) {
 	SDL_Rect rect;
 	rect.x = (WIDTH / 2) + i * ((WIDTH / 2) / RAYS); // Draw upper half
 	rect.y = HEIGHT / 2;
 	rect.w = (WIDTH / 2) / RAYS;
-	rect.h = (MAPX * MAPY * (HEIGHT / 2)) / len;
+	rect.h = 0.5 * (MAPX * MAPY * (HEIGHT / 2)) / len;
 	if (rect.h > HEIGHT / 2) {
 		rect.h = HEIGHT / 2;
 	}
@@ -25,41 +61,31 @@ void drawRay3D(SDL_Renderer *renderer, int i, int len) {
 	rect.h = -rect.h; // Draw lower half
 	SDL_RenderFillRect(renderer, &rect);
 }
-void checkAngleLimits(float *angle) {
-	if (*angle < 0) {
-		*angle += 2 * PI;
-	}
-	if (*angle > 2 * PI) {
-		*angle -= 2 * PI;
-	}
-}
 void horizontalRay(mvsqr player, float rayAngle, int map[], float *rayHX, float *rayHY, float *hLen, int *hColor) {
 	int dof = 1;
 	int mapLineSpacing = HEIGHT / MAPY;
 	int playerLine = player.y / mapLineSpacing;
-	int nextMapLine = playerLine;
+	int nextMapLine = playerLine + 1;
 	int rayMapPosX, rayMapPosY, rayMapPos;
-	while (dof < 8) {
-		if (rayAngle > PI) { // Looking up
-			if (dof != 1) {
-				nextMapLine--;
-			}
-			*rayHY = nextMapLine * mapLineSpacing - 0.001; // ray is "inside" the tile above
-			*rayHX = player.x - (player.y - *rayHY) / tan(rayAngle);
+	while (dof < DOF) {
+		if (rayAngle > PI) { // Facing up
+			nextMapLine--;
+			*rayHY = nextMapLine * mapLineSpacing;
+			rayMapPosY = nextMapLine - 1;
 			*hLen = fabs((player.y - *rayHY) / sin(rayAngle));
 		}
-		if (rayAngle == PI || rayAngle == 0) { // Looking straight left or right
+		if (rayAngle > 0 && rayAngle < PI) { // Facing down
+			*rayHY = nextMapLine * mapLineSpacing;
+			rayMapPosY = nextMapLine;
+			*hLen = fabs((*rayHY - player.y) / sin(rayAngle));
+			nextMapLine++;
+		}
+		if (rayAngle == PI || rayAngle == 0) { // Facing straight left or right
 			*hLen = 9001;
 			break;
 		}
-		if (rayAngle > 0 && rayAngle < PI) { // Looking down
-			nextMapLine++;
-			*rayHY = nextMapLine * mapLineSpacing;
-			*rayHX = player.x - (player.y - *rayHY) / tan(rayAngle);
-			*hLen = fabs((*rayHY - player.y) / sin(rayAngle));
-		}
+		*rayHX = player.x - (player.y - *rayHY) / tan(rayAngle);
 		rayMapPosX = *rayHX / mapLineSpacing;
-		rayMapPosY = *rayHY / mapLineSpacing;
 		rayMapPos = rayMapPosY * MAPX + rayMapPosX;
 		if (rayMapPos >= MAPX * MAPY - 1 || rayMapPos < 0) {
 			break;
@@ -75,28 +101,26 @@ void verticalRay(mvsqr player, float rayAngle, int map[], float *rayVX, float *r
 	int dof = 1;
 	int mapLineSpacing = (WIDTH / 2) / MAPX;
 	int playerLine = player.x / mapLineSpacing;
-	int nextMapLine = playerLine;
+	int nextMapLine = playerLine + 1;
 	int rayMapPosX, rayMapPosY, rayMapPos;
-	while (dof < 8) {
-		if (rayAngle > 3 * PI / 2 || rayAngle < PI / 2) { // Looking right
-			nextMapLine++;
+	while (dof < DOF) {
+		if (rayAngle > PI / 2 && rayAngle < 3 * PI / 2) { // Facing left
+			nextMapLine--;
 			*rayVX = nextMapLine * mapLineSpacing;
-			*rayVY = tan(rayAngle) * (*rayVX - player.x) + player.y;
-			*vLen = fabs((*rayVX - player.x) / cos(rayAngle));
+			rayMapPosX = nextMapLine - 1;
+			*vLen = fabs((player.x - *rayVX) / cos(rayAngle));
 		}
-		if (rayAngle == PI / 2 || rayAngle == 3 * PI / 2) { // Looking straight up or down
+		if (rayAngle > 3 * PI / 2 || rayAngle < PI / 2) { // Facing right
+			*rayVX = nextMapLine * mapLineSpacing;
+			rayMapPosX = nextMapLine;
+			*vLen = fabs((*rayVX - player.x) / cos(rayAngle));
+			nextMapLine++;
+		}
+		if (rayAngle == PI / 2 || rayAngle == 3 * PI / 2) { // Facing straight up or down
 			*vLen = 9001;
 			break;
 		}
-		if (rayAngle > PI / 2 && rayAngle < 3 * PI / 2) { // Looking left
-			if (dof != 1) {
-				nextMapLine--;
-			}
-			*rayVX = nextMapLine * mapLineSpacing - 0.001; // ray is "inside" the left tile
-			*rayVY = tan(rayAngle) * (*rayVX - player.x) + player.y;
-			*vLen = fabs((player.x - *rayVX) / cos(rayAngle));
-		}
-		rayMapPosX = *rayVX / mapLineSpacing;
+		*rayVY = tan(rayAngle) * (*rayVX - player.x) + player.y;
 		rayMapPosY = *rayVY / mapLineSpacing;
 		rayMapPos = rayMapPosY * MAPX + rayMapPosX;
 		if (rayMapPos >= MAPX * MAPY - 1 || rayMapPos < 0) {
@@ -129,34 +153,14 @@ void drawRays(SDL_Renderer *renderer, mvsqr player, int map[]) {
 			shortColor = hColor;
 			shade = 0.6;
 		}
-		switch (shortColor) {
-		case 1:
-			SDL_SetRenderDrawColor(renderer, 255 * shade, 0, 255 * shade, SDL_ALPHA_OPAQUE);
-			break;
-		case 2:
-			SDL_SetRenderDrawColor(renderer, 0, 0, 255 * shade, SDL_ALPHA_OPAQUE);
-			break;
-		case 3:
-			SDL_SetRenderDrawColor(renderer, 0, 255 * shade, 0, SDL_ALPHA_OPAQUE);
-			break;
-		}
+		setColor(renderer, shortColor, shade);
 
-		// Fix fisheye effect
-		float playerRayAngle = player.angle - rayAngle;
+		float playerRayAngle = player.angle - rayAngle; // Fix fisheye effect
 		checkAngleLimits(&playerRayAngle);
 		shortLen = shortLen * cos(playerRayAngle); // Math magic
 		drawRay3D(renderer, i, shortLen);
 		rayAngle += FOV / (RAYS - 1);
 	}
-}
-void drawPlayer(SDL_Renderer *renderer, mvsqr player) {
-	SDL_Rect playerRect;
-	playerRect.x = player.x - player.width / 2;
-	playerRect.y = player.y - player.width / 2;
-	playerRect.w = player.width;
-	playerRect.h = player.width;
-	SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
-	SDL_RenderFillRect(renderer, &playerRect);
 }
 void draw3DBackground(SDL_Renderer *renderer) {
 	SDL_Rect rect;
@@ -168,7 +172,7 @@ void draw3DBackground(SDL_Renderer *renderer) {
 	SDL_RenderFillRect(renderer, &rect);
 	rect.y = HEIGHT / 2;
 	rect.h = HEIGHT;
-	SDL_SetRenderDrawColor(renderer, 0, 51, 0, SDL_ALPHA_OPAQUE);
+	SDL_SetRenderDrawColor(renderer, 129, 82, 0, SDL_ALPHA_OPAQUE);
 	SDL_RenderFillRect(renderer, &rect);
 }
 void draw2DBackground(SDL_Renderer *renderer, int map[]) {
@@ -179,26 +183,36 @@ void draw2DBackground(SDL_Renderer *renderer, int map[]) {
 			rect.y = y * (HEIGHT / MAPY);
 			rect.w = ((WIDTH / 2) / MAPX) - 2;
 			rect.h = (HEIGHT / MAPY) - 2;
-			switch (map[y * MAPX + x]) {
-			case 0:
-				SDL_SetRenderDrawColor(renderer, 0, 51, 0, SDL_ALPHA_OPAQUE);
-				break;
-			case 1:
-				SDL_SetRenderDrawColor(renderer, 255, 0, 255, SDL_ALPHA_OPAQUE);
-				break;
-			case 2:
-				SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
-				break;
-			case 3:
-				SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
-				break;
-			}
+			setColor(renderer, map[y * MAPX + x], 1);
 			SDL_RenderFillRect(renderer, &rect);
 		}
 	}
 }
+void drawPlayer(SDL_Renderer *renderer, mvsqr player) {
+	SDL_Rect playerRect;
+	playerRect.x = player.x - player.width / 2;
+	playerRect.y = player.y - player.width / 2;
+	playerRect.w = player.width;
+	playerRect.h = player.width;
+	SDL_SetRenderDrawColor(renderer, 0, 0, 255, SDL_ALPHA_OPAQUE);
+	SDL_RenderFillRect(renderer, &playerRect);
+}
 int playerInWall(int playerX, int playerY, int map[]) {
 	return map[playerY / (HEIGHT / MAPY) * MAPX + playerX / ((WIDTH / 2) / MAPX)];
+}
+void movePlayer(mvsqr *player, int map[], int forward) {
+	player->x += forward * player->deltaX;
+	player->y += forward * player->deltaY;
+	if (player->x > WIDTH / 2 || player->x < 0 || playerInWall(player->x, player->y, map)) {
+		player->x -= forward * player->deltaX;
+		player->y -= forward * player->deltaY;
+	}
+}
+void rotatePlayer(mvsqr *player, int right) {
+	player->angle += right * 0.02;
+	checkAngleLimits(&player->angle);
+	player->deltaX = cos(player->angle) * 2.5;
+	player->deltaY = sin(player->angle) * 2.5;
 }
 int handleInputs(mvsqr *player, int map[]) {
 	SDL_Event event;
@@ -209,60 +223,40 @@ int handleInputs(mvsqr *player, int map[]) {
 		}
 	}
 	if (currentKeyStates[SDL_SCANCODE_W]) {
-		player->x += player->deltaX;
-		player->y += player->deltaY;
-		if (player->x > WIDTH / 2 || player->x < 0 ||
-			playerInWall(player->x, player->y, map)) {
-			player->x -= player->deltaX;
-			player->y -= player->deltaY;
-		}
+		movePlayer(player, map, 1);
 	}
 	if (currentKeyStates[SDL_SCANCODE_S]) {
-		player->x -= player->deltaX;
-		player->y -= player->deltaY;
-		if (player->x > WIDTH / 2 || player->x < 0 ||
-			playerInWall(player->x, player->y, map)) {
-			player->x += player->deltaX;
-			player->y += player->deltaY;
-		}
-	}
-	if (currentKeyStates[SDL_SCANCODE_A]) {
-		player->angle -= 0.03;
-		checkAngleLimits(&player->angle);
-		player->deltaX = cos(player->angle) * 2.5;
-		player->deltaY = sin(player->angle) * 2.5;
+		movePlayer(player, map, -1);
 	}
 	if (currentKeyStates[SDL_SCANCODE_D]) {
-		player->angle += 0.03;
-		checkAngleLimits(&player->angle);
-		player->deltaX = cos(player->angle) * 2.5;
-		player->deltaY = sin(player->angle) * 2.5;
+		rotatePlayer(player, 1);
+	}
+	if (currentKeyStates[SDL_SCANCODE_A]) {
+		rotatePlayer(player, -1);
 	}
 	if (currentKeyStates[SDL_SCANCODE_ESCAPE]) {
 		return 0;
 	}
 	return 1;
 }
-int isMouseEvent(void *userdata, SDL_Event *event) { // Remove mouse movement from event handler
-	return event->type != SDL_MOUSEMOTION;
-}
 int main(int argc, char *argv[]) {
-	// 1->Pink, 2->Blue, 3->Green
+	// 1->Pink, 2->Blue, 3->Green, 4->White
 	int map[MAPY * MAPX] = {
-		1, 1, 1, 1, 1, 1, 1, 1,
-		1, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 2, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 3, 0, 1,
-		1, 0, 0, 2, 0, 0, 0, 1,
-		1, 0, 0, 0, 0, 0, 0, 1,
-		1, 1, 1, 1, 1, 1, 1, 1};
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 6, 0, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 5, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 2, 0, 0, 3, 0, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 0, 0, 0, 4, 0, 0, 7, 0, 1,
+		1, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
 	SDL_Window *window = NULL;
 	SDL_Renderer *renderer = NULL;
 	SDL_Init(SDL_INIT_VIDEO);
 	SDL_CreateWindowAndRenderer(WIDTH, HEIGHT, 0, &window, &renderer);
-	SDL_SetEventFilter(isMouseEvent, NULL);
-	mvsqr player = {10 + (WIDTH / 2) / MAPX, 10 + HEIGHT * 4 / MAPY, cos(0) * 2.5, sin(0) * 2.5, 0, 20};
+	mvsqr player = {10 + (WIDTH / 2) / MAPX, 10 + HEIGHT * 4 / MAPY, cos(0.2) * 2.5, sin(0.2) * 2.5, 0.2, 20};
 	while (handleInputs(&player, map)) {
 		SDL_SetRenderDrawColor(renderer, 127, 127, 127, SDL_ALPHA_OPAQUE);
 		SDL_RenderClear(renderer);
